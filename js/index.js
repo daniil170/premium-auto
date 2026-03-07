f6973dcc57d259f821692ee486cf173d556914b8
Daniil <daniilivakin30@gmail.com>
2026-03-07 13:33:46 +0500

    fix mobile versin nav-links

--- js/index.js
+++ js/index.js
@@ -3,3 +3,10 @@ alert ("When you click on the links to cars, you will be redirected to this comp
 function closeModal() {
     document.getElementById('modalOverlay').style.display = 'none';
 }
+
+const burger = document.getElementById('burger');
+const nav = document.querySelector('.nav');
+
+burger.addEventListener('click', () => {
+    nav.classList.toggle('open');
+});
--- js/ruindex.js
+++ js/ruindex.js
@@ -3,3 +3,10 @@ alert ("Когда вы переходите по ссылкам на автом
 function closeModal() {
     document.getElementById('modalOverlay').style.display = 'none';
 }
+
+const burger = document.getElementById('burger');
+const nav = document.querySelector('.nav');
+
+burger.addEventListener('click', () => {
+    nav.classList.toggle('open');
+});
