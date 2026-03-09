db7c48d4edaa9a7013c5aa4dce3b7bf666ff42ff
Daniil <daniilivakin30@gmail.com>
2026-03-09 21:39:49 +0500

    add russian version about page and media

--- js/index.js
+++ js/index.js
@@ -1,12 +1,15 @@
-alert ("When you click on the links to cars, you will be redirected to this company's private website.")
+if (!sessionStorage.getItem('modalShown')) {
+    document.getElementById('modalOverlay').style.display = 'flex';
+    sessionStorage.setItem('modalShown', 'true');
+} else {
+    document.getElementById('modalOverlay').style.display = 'none';
+}
 
 function closeModal() {
     document.getElementById('modalOverlay').style.display = 'none';
 }
 
-const burger = document.getElementById('burger');
-const nav = document.querySelector('.nav');
-
-burger.addEventListener('click', () => {
-    nav.classList.toggle('open');
-});
+if (!sessionStorage.getItem('alertShown')) {
+    alert("When you click on the links to cars, you will be redirected to this company's private website.");
+    sessionStorage.setItem('alertShown', 'true');
+}
--- js/ruindex.js
+++ js/ruindex.js
@@ -1,12 +1,15 @@
-alert ("Когда вы переходите по ссылкам на автомобили, вы переходите на частный сайт этой компании.")
+if (!sessionStorage.getItem('modalShown')) {
+    document.getElementById('modalOverlay').style.display = 'flex';
+    sessionStorage.setItem('modalShown', 'true');
+} else {
+    document.getElementById('modalOverlay').style.display = 'none';
+}
 
 function closeModal() {
     document.getElementById('modalOverlay').style.display = 'none';
 }
 
-const burger = document.getElementById('burger');
-const nav = document.querySelector('.nav');
-
-burger.addEventListener('click', () => {
-    nav.classList.toggle('open');
-});
+if (!sessionStorage.getItem('alertShown')) {
+    alert("Когда вы переходите по ссылкам на автомобили, вы переходите на частный сайт этой компании.");
+    sessionStorage.setItem('alertShown', 'true');
+}
